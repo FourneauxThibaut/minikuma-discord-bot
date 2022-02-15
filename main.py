56f3db6c71548f1ab15a4dbc76399d3faa4b536f
Thibaut Fourneaux <fourneaux.thibaut@gmail.com>
2022-02-15 20:28:17 +0100

    help, join and leave is working

--- main.py
+++ main.py
@@ -1,32 +1,47 @@
-import os
 import discord
-from discord.ext import commands
+import os
+# load our local env so we dont have the token in public
 from dotenv import load_dotenv
+from discord.ext import commands
+from discord.utils import get
+from discord import FFmpegPCMAudio
+from discord import TextChannel
+from youtube_dl import YoutubeDL
 
 load_dotenv()
+
 class MiniKuma(commands.Bot):
+    is_connected = False
+
     def __init__(self):
-        super().__init__(command_prefix=["mk:", "Mk:", "mK:", "MK:", "mk/", "Mk/", "mK/", "MK/"])
+        super().__init__(command_prefix=[".mk ",";mk ",">.mk ", ".Mk ",";Mk ",">.Mk ", ".mK ",";mK ",">.mK ", ".MK ",";MK ",">.MK ",], help_command=None)
+
+        @self.command(name='help')                                                                      # mk:join
+        async def send_help(ctx):
+            await ctx.send("to make the bot join the vocal type:  .mk join")
+            await ctx.send("to make the bot leave type: .mk leave")
 
         @self.command(name='join')                                                                      # mk:join
         async def join_channel(ctx):
-            channel = ctx.author.voice.channel
-            if channel is not None:
-                voice_client = discord.utils.get(bot.voice_clients, guild=ctx.guild)
-                if voice_client == channel:
-                    await ctx.send("I am alredy in this voice channel")
-                elif voice_client != channel:
-                    leave_channel(ctx)
-                    await channel.connect()
-                else:
-                    await channel.connect()
-            else:                
+
+            if ctx.author.voice is None:
                 await ctx.send("You must be in a voice channel first so I can join it.")
-        
+            else:
+                channel = ctx.author.voice.channel
+                if self.is_connected == False:
+                    await channel.connect()
+                    self.is_connected = True
+                    print(f"connected to {channel} in {ctx.guild}")
+                else:
+                    await ctx.guild.voice_client.disconnect()
+                    await channel.connect()
+                    print(f"MiniKuma moved to {channel} in {ctx.guild}")
+  
         @self.command(name='leave')                                                                      # mk:leave
         async def leave_channel(ctx):
             if (ctx.voice_client):                              # If the bot is in a voice channel 
                 await ctx.guild.voice_client.disconnect()
+                self.is_connected = False
             else:                                               # But if it isn't
                 await ctx.send("I'm not in a voice channel, use the join command to make me join")
 
@@ -39,7 +54,6 @@ class MiniKuma(commands.Bot):
             guild_count = guild_count + 1
 
         print("MiniKuma is in " + str(guild_count) + " guilds.")
-
 
 bot = MiniKuma()
 bot.run(os.getenv('DISCORD_BOT_TOKEN'))
